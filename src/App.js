f85251b0cc8616d4442881d7360d71d329cd5fc5
niksankarkee <karkeeniksan@gmail.com>
2021-11-17 22:27:38 +0900

    state and props practise completed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import AddUser from './components/Users/AddUser';
+import UserList from './components/Users/UserList';
 
 function App() {
-  return <div>App</div>;
+  const [usersList, setUserList] = useState([]);
+
+  const addUserHandler = (username, age) => {
+    setUserList((prevList) => {
+      return [
+        ...prevList,
+        { name: username, age, id: Math.random().toString() },
+      ];
+    });
+  };
+
+  return (
+    <div>
+      <AddUser onAddUser={addUserHandler} />
+      <UserList users={usersList} />
+    </div>
+  );
 }
 
 export default App;
