e19ce3902329c4c3e789ad8781e1322fbc410281
niksankarkee <karkeeniksan@gmail.com>
2021-11-23 09:37:05 +0900

    context api practised

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect, useReducer } from 'react';
+import React, { useState, useEffect, useReducer, useContext } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
+import AuthContext from '../../store/auth-context';
 
 const emailInput = 'EMAIL_INPUT';
 const emailBlur = 'EMAIL_BLUR';
@@ -55,6 +56,8 @@ const Login = (props) => {
   const { isValid: emailIsValid } = emailState;
   const { isValid: passwordIsValid } = passwordState;
 
+  const authCtx = useContext(AuthContext);
+
   useEffect(() => {
     const identifer = setTimeout(() => {
       console.log('checking form validity');
@@ -89,7 +92,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    authCtx.onLogin(emailState.value, passwordState.value);
   };
 
   return (
