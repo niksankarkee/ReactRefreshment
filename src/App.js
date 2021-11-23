f7ef42253e1c30f362b6d136231eba2838219a25
niksankarkee <karkeeniksan@gmail.com>
2021-11-23 11:43:44 +0900

    all component and style setup with portal

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import Cart from './components/Cart/Cart';
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
 
 function App() {
   return (
-    <div>
-      <h2>Lets get started!</h2>
-    </div>
+    <React.Fragment>
+      <Cart />
+      <Header />
+      <main>
+        <Meals />
+      </main>
+    </React.Fragment>
   );
 }
 
